1d36cc8088bf7b2585d8d90e7ea777313bf74b8c
Georg Cederborg <57774121+gceder@users.noreply.github.com>
2023-04-24 11:04:00 -0600

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -1,31 +1,30 @@
 chrome.declarativeNetRequest.onHeadersReceived.addListener(function(details) {
-// Get the URL of the current request.
-var url = details.url;
+  if (chrome.declarativeNetRequest) {
+    // Get the URL of the current request.
+    var url = details.url;
 
-// Analyze the speed performance of the website.
-var lighthouse = new Lighthouse();
-lighthouse.run(url).then(function(result) {
-// Get the performance metrics.
-var performanceMetrics = result.performance;
-// Get the recommendations.
-var recommendations = result.categories.performance.remediations;
+    // Analyze the speed performance of the website.
+    var lighthouse = new Lighthouse();
+    lighthouse.run(url).then(function(result) {
+      // Get the performance metrics.
+      var performanceMetrics = result.performance;
+      // Get the recommendations.
+      var recommendations = result.categories.performance.remediations;
 
-// Create a PDF with the results.
-var pdf = new PDF();
-pdf.addPage(new Paragraph("Website Speed Analyzer"));
-pdf.addPage(new Paragraph("Performance Metrics"));
-for (var key in performanceMetrics) {
-  pdf.addParagraph(key + ": " + performanceMetrics[key]);
-}
-pdf.addPage(new Paragraph("Recommendations"));
-for (var key in recommendations) {
-  pdf.addParagraph(key + ": " + recommendations[key]);
-}
+      // Create a PDF with the results.
+      var pdf = new PDF();
+      pdf.addPage(new Paragraph("Website Speed Analyzer"));
+      pdf.addPage(new Paragraph("Performance Metrics"));
+      for (var key in performanceMetrics) {
+        pdf.addParagraph(key + ": " + performanceMetrics[key]);
+      }
+      pdf.addPage(new Paragraph("Recommendations"));
+      for (var key in recommendations) {
+        pdf.addParagraph(key + ": " + recommendations[key]);
+      }
 
-// Save the PDF to the user's downloads folder.
-pdf.save("website-speed-analyzer.pdf");
-});
-}, {
-urls: [":///*"],
-types: ["main_frame"]
+      // Save the PDF to the user's downloads folder.
+      pdf.save("website-speed-analyzer.pdf");
+    });
+  }
 });
